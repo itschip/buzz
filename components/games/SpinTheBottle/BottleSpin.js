83be2a36af2b830776a130222be48c6e3c4beda1
chip <onlygamernerd@gmail.com>
2020-09-22 00:29:49 +0200

    New function

--- components/games/SpinTheBottle/BottleSpin.js
+++ components/games/SpinTheBottle/BottleSpin.js
@@ -4,12 +4,17 @@ import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native'
 export const BottleSpin = () => {
   const [text, setText] = useState('Bottle Spin')
 
+
+  const onBottleSpin = () => {
+    console.log('Skkrrt')
+  }
+
   return (
     <View>
       <View style={styles.imgContainer}>
         <Image style={styles.bottle} source={require('../../../assets/beerbottle.png')} />
       </View>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={onBottleSpin}>
         <Text>Spin the bottle</Text>
       </TouchableOpacity>
     </View>
